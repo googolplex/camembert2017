7a48174b2204580c4c4bf7c3ccc4e7a05f42ad06
googol plex <googolplex@lcompras.biz>
2017-08-08 13:20:22 -0400

    habilito el proyecto camembert

---
+++ gitfeliz/clonar_camembert2017.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git clone https://github.com/googolplex/camembert2017.git
---
+++ src/biz/lcompras/ServicioPayment.java
@@ -0,0 +1,22 @@
+package biz.lcompras;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import org.json.JSONException;
+@Path("/payment")
+public class ServicioPayment {
+	private static final long serialVersionUID = 1L;
+    @GET
+    @Produces("application/json")
+    public Response getUsers() throws JSONException {
+            List<Payment> users = new ArrayList<>();
+            users.add(new Payment("admin"));
+            users.add(new Payment("john"));
+            users.add(new Payment("usuario2"));
+            return Response.status(200).entity(users.toString()).build();
+    }
+	
+}
